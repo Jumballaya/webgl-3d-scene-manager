baed3688db246e7553ebb1fc7fccd949889bfcb7
Jumballaya <nchupacabra@gmail.com>
2024-04-28 17:21:53 -0500

    set up github actions to deploy to ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? undefined : '/webgl-3d-scene-manager/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+}));
